1490dd20aaac4054ab03f5974fc8ddb09c135399
Celso Dantas <celsodantas@gmail.com>
2013-09-11 23:22:53 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Winnipegrb::Application.config.secret_token = 'ead061ff6824e1646cb53c1bff884172a86ddd1a8414b21bd1bb3a33d1b62fba021dc3b9819aa47acf6aa5a2a6f4f5b6287fdacfc20300016ca789daac287a78'
