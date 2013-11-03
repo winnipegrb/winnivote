67524f965712c6e0641df87fd30aaa549e4e1750
Amir Barylko <amir@barylko.com>
2013-11-03 17:33:25 -0600

    Added feature to list all ideas (failing)

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,5 @@
 FactoryGirl.define do 
   factory :user do
-    # sequence(:username) { |n| "foo#{n}" }
     password "foobar"
     password_confirmation { |u| u.password }
     sequence(:email) { |n| "foo#{n}@example.com" }
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,6 +3,7 @@ ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 require 'rspec/autorun'
+require 'factory_girl_rails'
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
@@ -35,4 +36,7 @@ RSpec.configure do |config|
   # the seed, which is printed after each run.
   #     --seed 1234
   config.order = "random"
+  
+  config.include FactoryGirl::Syntax::Methods
+  config.treat_symbols_as_metadata_keys_with_true_values = true  
 end
