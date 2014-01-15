75550e03d7d658d6c6fcaed982e6f369bcd8bde4
Florida Elago <florida.elago@gmail.com>
2014-01-14 19:58:30 -0600

    add create and new actions

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -2,4 +2,19 @@ class IdeasController < ApplicationController
   def index
     @ideas = Idea.all
   end
+
+  def new
+    @idea = Idea.new
+  end
+
+  def create
+    @idea = Idea.new(params["idea"])
+    if @idea.save
+      redirect_to :root
+    else
+      render "new"
+    end
+    
+  end
+
 end
