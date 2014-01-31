2829d5da0c20d35567c17c77c8698e95b09f56eb
Gannon McGibbon <gannon.mcgibbon@gmail.com>
2014-01-30 19:30:40 -0600

    Added user nav menu, fixed HTML validity issues, added nav tests

--- spec/features/list_ideas_spec.rb
+++ spec/features/list_ideas_spec.rb
@@ -46,6 +46,13 @@ feature "Access to ideas list", :js do
       page.should have_selector("form#new_user")
     end
 
+    it "Should have unauthenticated user nav" do
+      # check if page has user nav and the right subelements
+      page.should_not have_selector("nav.user ul li a", text: "Sign out")
+      page.should have_selector("nav.user ul li a", text: "Sign up")
+      page.should have_selector("nav.user ul li a", text: "Sign in")
+    end
+
   end
 
   context "When logged in"  do
@@ -58,7 +65,15 @@ feature "Access to ideas list", :js do
     it "Checks user was not redirected" do
       # check if not redirected
       current_path.should == root_path
-      page.should have_selector("p.notice")
+      page.should_not have_selector("form#new_user")
     end
+
+    it "Should have authenticated user nav" do
+      # check if page has user nav and the right subelements
+      page.should have_selector("nav.user ul li a", text: "Sign out")
+      page.should_not have_selector("nav.user ul li a", text: "Sign up")
+      page.should_not have_selector("nav.user ul li a", text: "Sign in")
+    end
+
   end
 end
