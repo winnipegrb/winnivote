f109d1bbf040a256c6a35d4e5c04b8f28aa345dd
Amir Barylko <amir@barylko.com>
2014-02-05 21:50:05 -0600

    Moved the login helpers to a separate file under support, added log_me_in in the features in the before block

--- spec/features/features_helper.rb
+++ spec/features/features_helper.rb
@@ -5,22 +5,13 @@ require 'factory_girl_rails'
 
 Dir[Rails.root.join("spec/features/support/**/*.rb")].each { |f| require f } 
 
-module FeatureHelpers
-  def login_as_user
-    user = FactoryGirl.create(:user)
-    login_as user, scope: :user
-    user
-  end
-end
-
-
 RSpec.configure do | config |
 
   config.include Rails.application.routes.url_helpers, :type => :feature
   config.include ActionView::Helpers::DateHelper
   config.include Warden::Test::Helpers
   config.include Capybara::DSL
-  config.include FeatureHelpers, :type => :feature
+  config.include LoginHelpers, :type => :feature
   
 end
 
---
+++ spec/features/sign_in_spec.rb
@@ -0,0 +1,47 @@
+require 'features/features_helper'
+
+feature "Sign in", :js do
+  
+  context "When not logged in" do
+
+    before do
+      visit(root_path)
+    end
+
+    it "Checks if redirect was successful" do
+      # check if redirected to sign in page
+      current_path.should == new_user_session_path
+      page.should have_selector("form#new_user")
+    end
+
+    it "Should have unauthenticated user nav" do
+      # check if page has user nav and the right subelements
+      page.should_not have_selector("nav.user ul li a", text: "Sign out")
+      page.should have_selector("nav.user ul li a", text: "Sign up")
+      page.should have_selector("nav.user ul li a", text: "Sign in")
+    end
+
+  end
+
+  context "When logged in"  do
+
+    before do
+      log_me_in
+      visit(root_path)
+    end
+    
+    it "Checks user was not redirected" do
+      # check if not redirected
+      current_path.should == root_path
+      page.should_not have_selector("form#new_user")
+    end
+
+    it "Should have authenticated user nav" do
+      # check if page has user nav and the right subelements
+      page.should have_selector("nav.user ul li a", text: "Sign out")
+      page.should_not have_selector("nav.user ul li a", text: "Sign up")
+      page.should_not have_selector("nav.user ul li a", text: "Sign in")
+    end
+
+  end
+end
