6529097c0056cbb397b3a1cb39316f48559d93a1
Gannon McGibbon <gannon.mcgibbon@gmail.com>
2014-03-02 19:45:15 -0600

    Fixed IdeasList error, added page object oriented feature to list_ideas_spec

--- spec/features/list_ideas_spec.rb
+++ spec/features/list_ideas_spec.rb
@@ -35,3 +35,36 @@ feature "List proposed ideas in order to vote", :js do
   end
 end
 
+feature "List proposed ideas in order to vote (SitePrism)", :js do
+  before do
+    log_me_in
+    @home = PageModels::Home.new
+  end
+
+  context "When there are no ideas to list" do
+    before do
+      @home.load
+    end
+
+    it "should not have ideas" do
+      @home.should have_idea_list
+      @home.idea_list.should_not have_ideas
+    end
+
+  end
+
+  context "When there are ideas to list" do
+
+    before do
+      FactoryGirl.create_list(:idea, 12)
+      @home.load
+    end
+
+    it "should have ideas" do
+      @home.should have_idea_list
+      @home.idea_list.should have_ideas count: 12
+    end
+    
+  end
+  
+end
