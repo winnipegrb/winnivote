fe8248c5c45520c4ef3b09a499745d95c520305c
James Partaker <twin891@gmail.com>
2014-04-16 13:02:40 -0500

    Convert add new project spec to use page object

--- spec/features/support/page_models/application_page.rb
+++ spec/features/support/page_models/application_page.rb
@@ -2,6 +2,7 @@ class ApplicationPage < SitePrism::Page
 
   class UserNav < SitePrism::Section
     element :winnivote_link, "a.navbar-brand", text: "Winnivote"
+    element :create_project_link, "ul.nav li a", text: "Create project"
     element :sign_in_link  , "ul.nav li a", text: "Sign In"
     element :sign_up_link  , "ul.nav li a", text: "Sign Up"
     element :sign_out_link , "ul.nav li a", text: "Sign Out"
